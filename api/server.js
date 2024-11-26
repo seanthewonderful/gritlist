891e297c44bddd861266413f720c7486a2d98707
Sean Fagan <faganseanp@gmail.com>
2024-11-26 16:09:23 -0700

    upodated with cors

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import fetchAndUpdateResults from "../public/search.js";
 import path from "path";
+import cors from "cors";
 
 const app = express();
 const port = 3333;
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(cors());
 
 app.post("/search", async (req, res) => {
     try {
